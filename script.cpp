5642e5db1177401d6b3ecfb3bc5ff3a9655512b2
Akshar Barchha <60133596+aksharbarchha@users.noreply.github.com>
2021-05-21 13:14:10 +0530

    Minor Changes

--- script.cpp
+++ script.cpp
@@ -9,7 +9,7 @@ int main(){
 	cout.tie(NULL);
     for(int i=1;i<=500;i++){
         cout<<"{ \"index\": { \"_id\": "<<i<<" }}"<<"\n";
-        cout<<"{\"business_address\":\""<<(315+i)<<" California St\",\"business_city\":\"San Francisco\",\"business_id\":\""<<(24936+i)<<"\""<<",\"business_longitude\":\""<<(-122.4+(double)i/10)<<"\",\"business_name\":\"San Francisco Soup Company"<<i<<"\",\"business_postal_code\":\""<<(94104+i)<<"\",\"business_state\":\"CA\",\"inspection_date\":\"2016-06-09T00:00:00.000\",\"inspection_id\":\""<<i<<"\",\"inspection_score\":"<<(77+i)<<",\"inspection_type\":\"Routine - Unscheduled\",\"risk_category\":\"Low Risk\",\"violation_description\":\"Improper food labeling or menu misrepresentation\",\"violation_id\":\""<<i<<"\"}"<<"\n";
+        cout<<"{\"business_address\":\""<<(315+i)<<" California St\",\"business_city\":\"San Francisco\",\"business_id\":\""<<(24936+i)<<"\""<<",\"business_longitude\":\""<<(-122.4+(double)i/10)<<"\",\"business_name\":\"San Francisco Soup Company"<<i<<"\",\"business_postal_code\":\""<<(94104+i)<<"\",\"business_state\":\"CA\",\"inspection_date\":\"2016-06-09T00:00:00+07:00\",\"inspection_id\":\""<<i<<"\",\"inspection_score\":"<<(77+i)<<",\"inspection_type\":\"Routine - Unscheduled\",\"risk_category\":\"Low Risk\",\"violation_description\":\"Improper food labeling or menu misrepresentation\",\"violation_id\":\""<<i<<"\"}"<<"\n";
 
     }
     return 0;

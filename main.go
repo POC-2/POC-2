a7f3728030cd8d3da3a00a9fafbacde9d1779159
Akshar Barchha <60133596+aksharbarchha@users.noreply.github.com>
2021-05-21 13:32:59 +0530

    Paginated Done

---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+	"net/http"
+	"github.com/olivere/elastic"
+	"github.com/gorilla/mux"
+	"strconv"
+	"log"
+)
+
+type Business struct {
+	Business_address      string    `json:"business_address"`
+	Business_city         string    `json:"business_city"`
+	Business_id           string    `json:"business_id"`
+	Business_name         string    `json:"business_name"`
+	Business_longitude    string    `json:"business_longitude"`
+	Business_postal_code  string    `json:"business_postal_code"`
+	Business_state        string    `json:"business_state"`
+	Inspection_date       time.Time `json:"inspection_date"`
+	Inspection_id         string    `json:"inspection_id"`
+	Inspection_score      int64     `json:"inspection_score"`
+	Inspection_type       string    `json:"inspection_type"`
+	Risk_category         string    `json:"risk_category"`
+	Violation_description string    `json:"violation_description"`
+	Violation_id          string    `json:"violation_id"`
+}
+
+func GetESClient() (*elastic.Client, error) {
+
+	client, err := elastic.NewClient(elastic.SetURL("http://localhost:9200"),
+		elastic.SetSniff(false),
+		elastic.SetHealthcheck(false))
+
+	fmt.Println("ES initialized...")
+
+	return client, err
+}
+
+func paginateData(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    
+    fromval := vars["from"]
+	sizeval := vars["size"]
+	from, err1 := strconv.Atoi(fromval)
+	size, err2 := strconv.Atoi(sizeval)
+	if err1 != nil || err2 != nil{
+		fmt.Println("Error initializing : ", err1)
+		fmt.Println("Error initializing : ", err2)
+		panic("String to Int conversion fail ")
+	}
+	ctx := context.Background()
+	esclient, err := GetESClient()
+	if err != nil {
+		fmt.Println("Error initializing : ", err)
+		panic("Client fail ")
+	}
+
+	var businesses []Business
+
+	searchSource := elastic.NewSearchSource()
+	searchSource.Query(elastic.NewMatchAllQuery())
+
+	queryStr, err1 := searchSource.Source()
+	queryJs, err2 := json.Marshal(queryStr)
+
+	if err1 != nil || err2 != nil {
+		fmt.Println("[esclient][GetResponse]err during query marshal=", err1, err2)
+	}
+	fmt.Println("[esclient]Final ESQuery=\n", string(queryJs))
+
+	searchService := esclient.Search().Index("poc_two").SearchSource(searchSource).From(from).Size(size)
+
+	searchResult, err := searchService.Do(ctx)
+	if err != nil {
+		fmt.Println("[ProductsES][GetPIds]Error=", err)
+		return
+	}
+	// fmt.Print("hits: ", searchResult.Hits.Hits)
+	for _, hit := range searchResult.Hits.Hits {
+		var business Business
+		// fmt.Println("Hit source: ", hit.InnerHits)
+		err := json.Unmarshal(hit.Source, &business)
+		if err != nil {
+			fmt.Println("[Getting Businesses][Unmarshal] Err=", err)
+		}
+
+		businesses = append(businesses, business)
+		json.NewEncoder(w).Encode(hit)
+	}
+
+	if err != nil {
+		fmt.Println("Fetching business fail: ", err)
+	} else {
+		for _, s := range businesses {
+			fmt.Printf("Businesses found Name: %s, Ins_id: %s, Vio_id: %s \n", s.Business_name, s.Inspection_id, s.Violation_id)
+		}
+	}
+
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/paginate/{from}/{size}", paginateData)
+	log.Fatal(http.ListenAndServe(":8090", myRouter))
+}
+
+func main() {
+	handleRequests()
+}
+
+
